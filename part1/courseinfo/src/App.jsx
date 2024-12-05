09b6c81ff35406c2a5550b8c841d7211d9173ea9
Antony <13antonysun@gmail.com>
2024-12-05 22:27:58 +0530

    exercise 1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,11 +1,50 @@
+const Header = (props) => {
+
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
 const App = () => {
 
   const course = 'Half Stack application development'
 
   const part1 = 'Fundamentals of react'
 
+  const exercises1 = 10
+
+  const part2 = 'Using props to pass data'
+
+  const exercises2 = 7
+
+  const part3 = 'State of a component'
+
+  const exercises3 = 14
+
   return (
-    <h1>Hello React bhai</h1>
+    <>
+      <Header course={course}/>
+      <Content part={part1} exercise={exercises1}/>
+      <Content part={part2} exercise={exercises2}/>
+      <Content part={part3} exercise={exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </>
   )
 }
 
