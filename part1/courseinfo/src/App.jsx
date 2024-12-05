bd71633a9b55179de47ba603ef1dedea489117ae
Antony <13antonysun@gmail.com>
2024-12-05 20:48:30 +0530

    Initial Commit

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+
+  const course = 'Half Stack application development'
+
+  const part1 = 'Fundamentals of react'
+
+  return (
+    <h1>Hello React bhai</h1>
+  )
+}
+
+export default App
