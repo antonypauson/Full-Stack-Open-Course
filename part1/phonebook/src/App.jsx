7ceda2f222dfdd6c01caa747a5a7cec9ed75bb82
Antony <13antonysun@gmail.com>
2024-12-18 12:41:07 +0530

    2.11

--- part1/phonebook/src/App.jsx
+++ part1/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { useEffect } from "react"
+import axios from 'axios'
 
 const Filter = ({onChange}) => {
   return (
@@ -28,15 +30,20 @@ const PersonForm = ({onSubmit, value1, value2, onChange1, onChange2}) => {
 
 const App = () => {
   
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [filteredNames, setFilteredNames] = useState(persons)
+
+  useEffect(() => {
+    console.log('effect')
+    axios 
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fullfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleChange = (event) => {
     setNewName(event.target.value)
